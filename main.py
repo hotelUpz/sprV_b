76520d4ec101083e6ed54149224a33c54e67a829
kali <kali@kali.netis.cc>
2025-06-11 02:49:20 +0300

    add VERSEWORLD

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ ADDRESSES_DATA = {
     # "ZERO_USDT": ('linea', '0x0040f36784dda0821e74ba67f86e084d70d67a3a'),
     "JAGER_USDT": ('bsc', '0x589e1c953bcb822a2094fd8c7cbbd84a7762fb04'),
     "BUBB_USDT": ('bsc','0xc8255e3fa0f4c6e6678807d663f9e2263e23a8e8'),
+    "VERSEWORLD_USDT": ('solana','6ltbm9gxmtmff1vy6jev5u3urmcsgtsuquhfnwwtmtli'),
 }
-SYMBOLS = ["TIBBIR_USDT", "BUBB_USDT"]
+SYMBOLS = ["TIBBIR_USDT", "BUBB_USDT", "VERSEWORLD_USDT"]
 
 # Timing:
 interval_map = {
@@ -33,7 +34,7 @@ TEXT_REFRESH_INTERVAL = interval_map["5m"]
 PRICE_REFRESH_INTERVAL = int(len(SYMBOLS)* 2.5)
 
 # Strayegy:
-WINDOW = 576 # minute
+WINDOW = 288 # minute
 HIST_SPREAD_LIMIT = 600
 DIRECTION_MODE = 3 # 1 -- Long only, 2 -- Short only, 3 -- Long + Short:
 DEVIATION = 0.89 # hvh
@@ -48,6 +49,11 @@ FIXED_THRESHOLD = {
         "long_val": -3.0, # %
         "short_val": 3.0 # %
     },
+    "VERSEWORLD_USDT": {
+        "is_active": True,
+        "long_val": -3.0, # %
+        "short_val": 5.0 # %
+    },
     "JAGER_USDT": {
         "is_active": True,
         "long_val": -3.5, # %
@@ -58,7 +64,7 @@ EXIT_THRESHOLD = 0.5
 CALC_SPREAD_METHOD = 'a'
 
 # Utils:
-PLOT_WINDOW = 576 # minute
+PLOT_WINDOW = 288 # minute
 MAX_RECONNECT_ATTEMPTS = 21
 
         
